baca14e48b42cf71ff00dc26440a2fa26325eb08
nnivxix <adhe.hanasa@gmail.com>
2022-04-29 17:02:09 +0700

    setup ts for vue project

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
+import HelloComponent from "./components/HelloComponent.vue";
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <h1 class="text-3xl font-bold text-white underline bg-blue-600 p-4">
     hello world
   </h1>
+  <HelloComponent />
 </template>
 
 <style>
